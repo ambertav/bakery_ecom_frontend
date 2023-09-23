0620fb566e15f2f7f0113d611373c0d1bc564081
Amber Taveras <taveras.amber@gmail.com>
2023-09-23 06:14:27 -0400

    conditionall renders if user

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,26 @@
-import { UserAuth } from '@/app/context/AuthContext';
+import { useAuth } from '../app/firebase/AuthContext';
+import Link from 'next/link';
 
 function Navigation () {
-    const { user } = UserAuth();
+    const { user } = useAuth();
     return (
         <nav>
-            <h1>This is the navigation bar</h1>
+            <ul>
+                {user ? (
+                    <>
+                        <Link href='/account'>Account</Link>
+                        <Link href='/cart'>Cart</Link>
+                        <Link href='/logout'>Logout</Link>
+                    </>
+                ) : (
+                    <>
+                        <Link href='/signup'>Signup</Link>
+                        <Link href='/login'>Login</Link>
+                    </>
+                )}
+            </ul>
         </nav>
-    )
+    );
 }
 
 export default Navigation;
