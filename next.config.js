f7e6b2ffa4833f33af5927393a0d9e504f557dad
Amber Taveras <taveras.amber@gmail.com>
2023-10-04 03:20:31 -0400

    process env for firebase variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    }
+}
 
 module.exports = nextConfig
