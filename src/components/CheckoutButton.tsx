f98673d48898971c667c5fe4debc0179515e78b6
Amber Taveras <taveras.amber@gmail.com>
2023-10-10 01:42:10 -0400

    fetches stripe session for checkout, redirects to stripe url

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -18,6 +18,19 @@ export default function CheckoutButton ({ cart } : CheckoutProps) {
 
     async function handleCheckout () {
         setIsLoading(true);
+        try {
+            const token = await getIdToken(user);
+            const response = await axios.post(url, { cart }, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            });
+            window.location = response.data.checkout_url;
+        } catch (error) {
+            console.error('Error creating Checkout session:', error);
+            setIsLoading(false);
+        }
     }
 
     return (
