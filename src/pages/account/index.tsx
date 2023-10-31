5ab8cca45dbec3a4e1c060457a834256824f2651
Amber Taveras <taveras.amber@gmail.com>
2023-10-30 22:08:48 -0400

    retrieves and renders orders

--- src/pages/account/index.tsx
+++ src/pages/account/index.tsx
@@ -1,17 +1,18 @@
 import axios from 'axios';
+import Link from 'next/link';
 import { getIdToken, User } from 'firebase/auth';
 import { useState, useEffect } from 'react';
 import { useAuth } from '@/app/firebase/AuthContext';
 
 import LoadingSpinner from '@/components/LoadingSpinner';
 
-import { AddressType } from '../../../types/types';
+import { AddressType, OrderType } from '../../../types/types';
 
 export default function Account () {
     const { user } = useAuth();
 
     const [ isLoading, setIsLoading ] = useState <boolean> (true);
-    const [ orders, setOrders ] = useState(null);
+    const [ orders, setOrders ] = useState <OrderType[] | null> (null);
     const [ addresses, setAddresses ] = useState <AddressType[] | null> (null);
 
     const url = 'http://127.0.0.1:5000/user/';
@@ -20,14 +21,13 @@ export default function Account () {
         const fetchOrders = async () => {
             if (user) {
                 try {
-                    const token = getIdToken(user);
+                    const token = await getIdToken(user);
                     const response = await axios.get(url + 'order/', {
                         headers: {
                             Authorization: `Bearer ${token}`,
                         },
                         withCredentials: true,
                     });
-                    console.log(response);
                     if (response.status === 200) setOrders(response.data.orders);
                 } catch (error) {
                     console.error('Error fetching order history: ', error);
@@ -45,17 +45,19 @@ export default function Account () {
                         withCredentials: true,
                     });
                     if (response.status === 200) {
-                        const { billAddress, shipAddress } = response.data || {}; // takes out all billing and shipping orders
-                        const allAddresses : AddressType[] = [...billAddress, ...shipAddress]; // merges types of addresses
-                        const addressSet = new Set(allAddresses.map(serializeAddress)); // removes duplicate addresses
+                        const { billAddress, shipAddress } = response.data || {}; 
+                        // merges types of addresses
+                        const allAddresses : AddressType[] = [...billAddress, ...shipAddress];
+                        // removes duplicate addresses
+                        const addressSet = new Set(allAddresses.map(serializeAddress));
                          // parse back to json, creates array from set, and assigns to state
                         setAddresses(Array.from(addressSet).map((str) => JSON.parse(str)));
                     }
                 } catch (error) {
                     console.error('Error fetching billing addresses:', error);
                 }
+            }
         }
-    }
         fetchOrders();
         fetchAddress();
         setTimeout(() => {
@@ -73,16 +75,17 @@ export default function Account () {
                 <h1>this is the account page</h1>
                 <div>
                     {orders ? (
-                        <div>
-<ul>
-  {/* {orders!.map((o, index) => (
-    <li key={index}>
-        <p>{o.date}</p>
-    </li>
-  ))} */}
-</ul>
-
-                        </div>
+                        <ul>
+                        {orders!.map((o, index) => (
+                            <li key={index}>
+                                <p>{ o.id }</p>
+                                <p>{ o.date }</p>
+                                <p>{ o.totalPrice }</p>
+                                <p>{ o.status }</p>
+                                <Link href='/account/orders/:id'></Link>
+                            </li>
+                        ))}
+                        </ul>
                     ) : (
                         'no orders'
                     )}
