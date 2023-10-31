74f6529a2abe07d883347af24d516739450cc856
Amber Taveras <taveras.amber@gmail.com>
2023-10-30 23:11:59 -0400

    prelim render of order history and detail

---
+++ src/pages/account/orders/[id].tsx
@@ -0,0 +1,7 @@
+export default function OrderDetail () {
+    return (
+        <main>
+            <h1>this is the order detail</h1>
+        </main>
+    )
+}
---
+++ src/pages/account/orders/index.tsx
@@ -0,0 +1,7 @@
+export default function OrderHistory () {
+    return (
+        <main>
+            <h1>this is the order history</h1>
+        </main>
+    )
+}
