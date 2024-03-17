e5b7ba7cec5d5d80bd473047577372b9d3e09523
Amber Taveras <taveras.amber@gmail.com>
2024-03-17 19:36:58 -0400

    implements filter by category on product index page

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,7 +1,7 @@
 import axios from '../../utilities/axiosConfig';
 import { useRouter } from 'next/router';
 import { useAuth } from '../../app/firebase/AuthContext';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, ChangeEvent } from 'react';
 import { ProductType } from '../../../types/types';
 
 import LoadingSpinner from '@/components/LoadingSpinner';
@@ -17,6 +17,7 @@ export default function ProductIndex() {
   const [currentPage, setCurrentPage] = useState<number>(
     Number(router.query.page) || 1
   );
+  const [category, setCategory] = useState<string>('');
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -24,10 +25,17 @@ export default function ProductIndex() {
 
     const fetchProducts = async () => {
       if (router.isReady) {
+        const categoryParam = category ? `category=${category}` : '';
+        const pageParam = `page=${router.query.page}`;
+
+        // combining paramaters
+        const queryParams = [categoryParam, pageParam].filter(param => param).join('&');
+    
+        // urlPath with query params
+        const urlPath = `product/?${queryParams}`;
+
         try {
-          const response = await axios.get(
-            `product/?page=${router.query.page}`
-          );
+          const response = await axios.get(urlPath);
           if (response.status === 200) {
             setProducts(response.data.products);
             setTotalPages(response.data.totalPages);
@@ -46,7 +54,7 @@ export default function ProductIndex() {
     fetchProducts();
 
     return () => clearTimeout(timeout);
-  }, [router.isReady, currentPage]);
+  }, [router.isReady, currentPage, router.query]);
 
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
@@ -57,13 +65,40 @@ export default function ProductIndex() {
     });
   };
 
+  const handleCategoryChange = (evt : ChangeEvent<HTMLSelectElement>) => {
+    setCategory(evt.target.value);
+    setIsLoading(true);
+    router.push({
+      pathname: router.pathname,
+      query: { ...router.query, category: evt.target.value },
+    });
+  }
+
   function loaded() {
     return (
       <main>
         <h1>All Products</h1>
         <div>
-          filter
-          <div>category sort by price asecending sort by price descending</div>
+        <div key={'category'}>
+            <label htmlFor={'category'}>Filter by Category</label>
+            <select
+              id="category"
+              name="category"
+              value={category}
+              onChange={handleCategoryChange}
+            >
+              <option value="">Select a category</option>
+              <option value="cake">Cake</option>
+              <option value="cupcake">Cupcake</option>
+              <option value="pie">Pie</option>
+              <option value="cookie">Cookie</option>
+              <option value="donut">Donut</option>
+              <option value="pastry">Pastry</option>
+            </select>
+          </div>
+        </div>
+        <div>
+        sort by price asecending sort by price descending
         </div>
         <div>search</div>
         <div>
