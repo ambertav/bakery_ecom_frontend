a69db06f1468c3b8b5b2e0205b2f8aeebdaa6ad3
Amber Taveras <taveras.amber@gmail.com>
2024-03-17 19:07:51 -0400

    renders and utilizes pagination comp., adjusts axios fetch w page info

--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,57 +1,98 @@
 import axios from '../../utilities/axiosConfig';
+import { useRouter } from 'next/router';
+import { useAuth } from '../../app/firebase/AuthContext';
 import { useState, useEffect } from 'react';
 import { ProductType } from '../../../types/types';
 
 import LoadingSpinner from '@/components/LoadingSpinner';
+import Pagination from '@/components/Pagination';
 import Product from '@/components/Product';
 
-export default function ProductIndex () {
-    const [ products, setProducts ] = useState<ProductType[] | null>(null);
-    const [ isLoading, setIsLoading ] = useState<boolean>(true);
+export default function ProductIndex() {
+  const router = useRouter();
+  const { isAdmin } = useAuth();
 
-    useEffect(() => {
-        const fetchProducts = async () => {
-            try {
-                const response = await axios.get('product');
-                if (response.status === 200) {
-                    const { products } = response.data;
-                    setProducts(products);
-                }
-            } catch (error) {
-                console.error('Error fetching products: ', error);
-            };
-        }
-        fetchProducts();
-        setTimeout(() => {
+  const [products, setProducts] = useState<ProductType[] | null>(null);
+  const [totalPages, setTotalPages] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(
+    Number(router.query.page) || 1
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout;
+
+    const fetchProducts = async () => {
+      if (router.isReady) {
+        try {
+          const response = await axios.get(
+            `product/?page=${router.query.page}`
+          );
+          if (response.status === 200) {
+            setProducts(response.data.products);
+            setTotalPages(response.data.totalPages);
+            setCurrentPage(response.data.currentPage);
+          }
+        } catch (error) {
+          console.error('Error fetching products: ', error);
+        } finally {
+          timeout = setTimeout(() => {
             setIsLoading(false);
-        }, 750);
-    }, []);
+          }, 500);
+        }
+      }
+    };
 
-    function loaded () {
-        return (
-            <main>
-                <h1>All Products</h1>
-                <div>
-                    <ul>
-                        {products !== null
-                            ? products.map((p, key) => (
-                                <div key={key}>
-                                    <Product product={p} page='index' /> 
-                                </div>
-                            ))
-                        : (
-                            <div>No products</div>
-                        )
-                        }
-                    </ul>
+    fetchProducts();
+
+    return () => clearTimeout(timeout);
+  }, [router.isReady, currentPage]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    setIsLoading(true);
+    router.push({
+      pathname: router.pathname,
+      query: { ...router.query, page: page },
+    });
+  };
+
+  function loaded() {
+    return (
+      <main>
+        <h1>All Products</h1>
+        <div>
+          filter
+          <div>category sort by price asecending sort by price descending</div>
+        </div>
+        <div>search</div>
+        <div>
+          <ul>
+            {products !== null ? (
+              products.map((p, key) => (
+                <div key={key}>
+                    <Product product={p} page="index" />
                 </div>
-        </main>
-        );
-    }
+              ))
+            ) : (
+              <div>No products</div>
+            )}
+          </ul>
+        </div>
+        <div>
+          <Pagination
+            totalPages={totalPages!}
+            currentPage={currentPage!}
+            onPageChange={handlePageChange}
+          />
+        </div>
+      </main>
+    );
+  }
 
-    function loading () {
-        return <LoadingSpinner />;
-    }
+  function loading() {
+    return <LoadingSpinner />;
+  }
 
-    return isLoading ? loading() : loaded();
+  return isLoading ? loading() : loaded();
 }
