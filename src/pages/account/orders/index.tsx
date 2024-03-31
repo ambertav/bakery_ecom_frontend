beb99ab4100f6b8a06ee11869906508e4328580b
Amber Taveras <taveras.amber@gmail.com>
2024-03-31 17:02:06 -0400

    adds pagination to user order history

--- src/pages/account/orders/index.tsx
+++ src/pages/account/orders/index.tsx
@@ -1,26 +1,41 @@
 import axios from '../../../utilities/axiosConfig';
+import { useRouter, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useAuth } from '@/app/firebase/AuthContext';
 
 import LoadingSpinner from '@/components/LoadingSpinner';
+import Pagination from '@/components/Pagination';
 
 import { OrderType } from '../../../../types/types';
 
 export default function OrderHistory() {
+  const router = useRouter();
+  const params = useSearchParams();
+
   const { user } = useAuth();
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [orders, setOrders] = useState<OrderType[] | null>(null);
+  const [orders, setOrders] = useState<OrderType[]>([]);
+  const [totalPages, setTotalPages] = useState<number | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(
+    Number(params?.get('page')) || 1
+  );
 
   useEffect(() => {
     let timeout: NodeJS.Timeout;
 
     const fetchOrders = async () => {
       if (user) {
+        const pageParam = `page=${params?.get('page')}`;
+
         try {
-          const response = await axios.get('order/');
-          if (response.status === 200) setOrders(response.data.orders);
+          const response = await axios.get(`order/?${pageParam}`);
+          if (response.status === 200) {
+            setOrders(response.data.orders);
+            setTotalPages(response.data.totalPages);
+            setCurrentPage(response.data.currentPage);
+          }
         } catch (error) {
           console.error('Error fetching order history: ', error);
         } finally {
@@ -30,17 +45,24 @@ export default function OrderHistory() {
         }
       }
     };
+
     fetchOrders();
 
     return () => clearTimeout(timeout);
-  }, []);
+  }, [params]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    setIsLoading(true);
+    router.push(`orders?page=${page}`);
+  };
 
   function loaded() {
     return (
       <main>
         <h1>Order History</h1>
         <div>
-          {orders ? (
+          {orders && (
             <ul>
               {orders!.map((o, index) => (
                 <li key={index}>
@@ -52,9 +74,14 @@ export default function OrderHistory() {
                 </li>
               ))}
             </ul>
-          ) : (
-            'no orders'
           )}
+        </div>
+        <div>
+          <Pagination
+            totalPages={totalPages!}
+            currentPage={currentPage!}
+            onPageChange={handlePageChange}
+          />
         </div>
       </main>
     );
