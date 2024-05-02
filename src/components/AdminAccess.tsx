0e998281cfdc95182995b92bbb4036050d9cde1f
Amber Taveras <taveras.amber@gmail.com>
2024-05-02 02:12:20 -0400

    creates admin access pages and signup functionality

---
+++ src/components/AdminAccess.tsx
@@ -0,0 +1,46 @@
+import { ChangeEvent, FormEvent, Dispatch } from 'react';
+import { renderInput } from '@/utilities/formUtilities';
+import { FormInput } from '../../types/types';
+
+interface UserAccessProps {
+    title : string;
+    errorMessage : string;
+    formInput : FormInput;
+    handleChange : Dispatch<ChangeEvent<HTMLInputElement>>
+    handleAdminSubmit : (formInput: FormInput) => Promise<void>;
+}
+
+export default function AdminAccess ({ title, errorMessage, formInput, handleChange, handleAdminSubmit } : UserAccessProps) {
+
+    const onSubmit = async (evt: FormEvent) => {
+        evt.preventDefault();
+        await handleAdminSubmit(formInput);
+    }
+
+    return (
+        <main>
+            <h1>{title}</h1>
+            <span>{errorMessage && errorMessage}</span>
+            <form onSubmit={onSubmit}>
+                {title === 'Admin Signup' && (
+                    <>
+                        {renderInput('name', 'Name', 'text', formInput, handleChange)}
+                        {renderInput('email', 'Email', 'email', formInput, handleChange)}
+                        {renderInput('password', 'Password', 'password', formInput, handleChange)}
+                        {renderInput('confirm_password', 'Confirm Password', 'password', formInput, handleChange)}
+                        {renderInput('pin', 'Pin', 'password', formInput, handleChange)}
+                        {renderInput('confirm_pin', 'Confirm Pin', 'password', formInput, handleChange)}
+                        {renderInput('employer_code', 'Employer Code', 'text', formInput, handleChange)}
+                    </>
+                )}
+                {title === 'Admin Login' && (
+                    <>
+                        {renderInput('email', 'Email', 'email', formInput, handleChange)}
+                        {renderInput('password', 'Password', 'password', formInput, handleChange)}
+                    </>
+                )}
+                <input type="submit" className="submit" value='Submit' />
+            </form>
+        </main>
+    );
+}
---
+++ src/pages/admin/signup.tsx
@@ -0,0 +1,22 @@
+import { createUserWithEmailAndPassword, UserCredential, User } from '@firebase/auth';
+import { auth } from '../../app/firebase/firebaseConfig';
+
+import Access from '@/components/Access';
+
+export default function Signup () {
+
+    async function signup (email : string, password : string) : Promise<User | null> {
+        try {
+            const userCredential : UserCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const newUser : User | null = userCredential.user;
+            return newUser;
+
+        } catch (error : any) {
+            throw error;
+        }
+    }
+      
+    return (
+        <Access method={signup} url='admin/signup/' resource='admin' />
+    );
+}
