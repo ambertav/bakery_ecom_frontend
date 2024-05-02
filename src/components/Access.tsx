3af4bd51c254350d930567ef98e9b2275e427b23
Amber Taveras <taveras.amber@gmail.com>
2024-05-02 02:10:59 -0400

    separates access func and render to account for use in user and admin pages

--- src/components/Access.tsx
+++ src/components/Access.tsx
@@ -1,31 +1,25 @@
 import axios from '../utilities/axiosConfig';
 import { FormInput } from "../../types/types";
-import { useState, ChangeEvent, FormEvent } from 'react';
+import { useState, ChangeEvent } from 'react';
 import { useRouter } from 'next/router';
 import { User } from '@firebase/auth';
-import { renderInput } from '@/utilities/formUtilities';
+import { handleFirebaseAuthErrors } from '../utilities/errors';
+
+import AdminAccess from './AdminAccess';
+import UserAccess from './UserAccess';
+import { validateForm } from '@/utilities/formUtilities';
 
 // type for signup and login props
 interface AccessProps {
     method: (email: string, password: string) => Promise<User | null>;
+    url: string;
+    resource: string;
   }
 
-export default function Access (props : AccessProps) {
+export default function Access ({ method, url, resource } : AccessProps) {
     const router = useRouter();
-    const [ formInput, setFormInput ] = useState<FormInput>({
-        name: '',
-        email: '',
-        password: '',
-        confirm_password: '',
-    });
+    const [ formInput, setFormInput ] = useState<FormInput>({});
     const [ errorMessage, setErrorMessage ] = useState<string>('');
-
-    function passwordVerification (password : string, confirm: string): string | undefined {
-        if (password !== confirm) return 'Passwords do not match';
-
-        // Return undefined when passwords match
-        return undefined;
-      }
 
     const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = evt.target;
@@ -35,31 +29,28 @@ export default function Access (props : AccessProps) {
         }));
     };
 
-    const handleSubmit = async (evt: FormEvent) => {
-        evt.preventDefault();
+    const handleUserSubmit = async (formInput : FormInput) => {
         setErrorMessage('');
         // password match for signup
         if (router.pathname === '/signup') {
-            const message = passwordVerification(formInput.password as string, formInput.confirm_password as string);
+            const message = await validateForm(formInput, ['password']);
             if (message) return setErrorMessage(message);
         }
         
         // if signup, send user name over to backend for creation, firebase_uid comes via token
-        const name = router.pathname === '/signup' ? formInput.name : '';
-        const localStorageCart = JSON.parse(localStorage.getItem('cart') || '[]');
-
         // passing in localStorageCart for cart item creation and association with user upon signup or login
-        const submitBody = { name, localStorageCart }
+        const submitBody = { 
+            name: router.pathname === '/signup' ? formInput.name : '',
+            localStorageCart: JSON.parse(localStorage.getItem('cart') || '[]')
+         }
 
         try {
             // signup or login user with firebase
-            const user = await props.method(formInput.email as string, formInput.password as string);
+            const user = await method(formInput.email as string, formInput.password as string);
             try {
                 if (user) {
-                    const response = await axios.post('user' + router.pathname, submitBody, { 
-                        headers: {
-                        'Content-Type': 'application/json',
-                        },
+                    const response = await axios.post(url, submitBody, { 
+                        headers: { 'Content-Type': 'application/json' },
                     });
                     // redirect to main page
                     if (response.status === 201 || response.status === 200)  {
@@ -71,33 +62,55 @@ export default function Access (props : AccessProps) {
                 console.error(error);
             }
         } catch (error : any) {
-            if (error.code === 'auth/email-already-in-use') setErrorMessage('Email already in use');
-            else if (error.code === 'auth/weak-password') setErrorMessage('Password should be at least 6 characters');
-            else if (error.code === 'auth/invalid-login-credentials') setErrorMessage('Invalid credentials, please try again');
+            setErrorMessage(handleFirebaseAuthErrors(error));
+        }
+    }
+
+    const handleAdminSubmit = async (formInput : FormInput) => {
+        setErrorMessage('');
+
+        // password match for signup
+        if (router.pathname === '/admin/signup') {
+            let message = await validateForm(formInput, ['password', 'pin', 'employerCode'])
+            if (message) return setErrorMessage(message);
+        }
+    
+        const submitBody = {
+            name: router.pathname === '/admin/signup' ? formInput.name : '',
+            pin: router.pathname === '/admin/signup' ? formInput.pin : '',
+            employerCode: router.pathname === '/admin/signup' ? formInput.employerCode : '',
+        };
+
+        try {
+            // signup or login user with firebase
+            const admin = await method(formInput.email as string, formInput.password as string);
+            try {
+                if (admin) {
+                    const response = await axios.post(url, submitBody, { 
+                        headers: { 'Content-Type': 'application/json' },
+                    });
+                    // redirect to main page
+                    if (response.status === 201 || response.status === 200)  {
+                        console.log(response.data.message);
+                        console.log(response.data.employeeId);
+                    }
+                }
+            } catch (error : any) {
+                console.error(error);
+            }
+        } catch (error : any) {
+            setErrorMessage(handleFirebaseAuthErrors(error));
         }
     }
 
     return (
-        <main>
-            <h1>{router.pathname === '/signup' ? 'Signup' : 'Login'}</h1>
-            <span>{errorMessage && errorMessage}</span>
-            <form onSubmit={handleSubmit}>
-                {router.pathname === '/signup' ? (
-                    <>
-                        {renderInput('name', 'Name', 'text', formInput, handleChange)}
-                        {renderInput('email', 'Email', 'email', formInput, handleChange)}
-                        {renderInput('password', 'Password', 'password', formInput, handleChange)}
-                        {renderInput('confirm_password', 'Confirm Password', 'password', formInput, handleChange)}
-                    </>
-                ) : (
-                    <>
-                        {renderInput('email', 'Email', 'email', formInput, handleChange)}
-                        {renderInput('password', 'Password', 'password', formInput, handleChange)}
-                    </>
-                )
-                }
-                <input type="submit" className="submit" value='Submit' />
-            </form>
-        </main>
+        <>
+            {resource === 'user' && (
+                <UserAccess title={router.pathname.substring(1)} errorMessage={errorMessage} formInput={formInput} handleChange={handleChange} handleUserSubmit={handleUserSubmit} />
+            )}
+            {resource === 'admin' && (
+                <AdminAccess title={router.pathname.substring(1).split('/').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')} errorMessage={errorMessage} formInput={formInput} handleChange={handleChange} handleAdminSubmit={handleAdminSubmit} />
+            )}
+        </>
     );
 }
