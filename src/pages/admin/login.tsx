2487d18db667199dc1c91de58e0f85809774512e
Amber Taveras <taveras.amber@gmail.com>
2024-05-12 22:32:16 -0400

    implements admin login and update pin pages

---
+++ src/pages/admin/login.tsx
@@ -0,0 +1,24 @@
+import { signInWithEmailAndPassword, UserCredential, User } from '@firebase/auth';
+import { auth } from '../../app/firebase/firebaseConfig';
+
+import Access from '@/components/Access';
+
+
+export default function Login () {
+
+    async function login (email : string, password : string) : Promise< User | null> {
+        try {
+            const userCredential : UserCredential = await signInWithEmailAndPassword(auth, email, password);
+            const currentUser : User | null = userCredential.user;
+            return currentUser;
+        } catch (error) {
+            throw error;
+        }
+    }    
+
+    return  (
+        <>
+            <Access method={login} url='admin/login/'  resource='admin'/>  
+        </>
+    );
+}
