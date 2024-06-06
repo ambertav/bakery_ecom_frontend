dc0e26f7549a5c5e046817cf89a45fa845125328
Amber Taveras <taveras.amber@gmail.com>
2024-06-06 13:18:41 -0400

    reworks checkout form to 2 steps, shipping / delivery, and billing info

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -3,72 +3,92 @@ import AddressForm from './AddressForm';
 
 import { CheckoutFormInput, AddressType } from '../../types/types';
 
-
 interface CheckoutFormProps {
-    formInput: CheckoutFormInput;
-    setFormInput: Dispatch<React.SetStateAction<CheckoutFormInput>>;
-    existingAddresses: AddressType[];
-    onSubmit: () => void;
-    isProcessing: boolean;
+  formInput: CheckoutFormInput;
+  setFormInput: Dispatch<React.SetStateAction<CheckoutFormInput>>;
+  existingAddresses: AddressType[];
+  onSubmit: () => void;
+  isProcessing: boolean;
 }
 
-export default function CheckoutForm ({ formInput, setFormInput, existingAddresses, onSubmit, isProcessing } : CheckoutFormProps) {
-    const [sameAsShipping, setSameAsShipping] = useState<boolean>(true);
-    const [showAddShipping, setShowAddShipping] = useState<boolean>(false);
-    const [showAddBilling, setShowAddBilling] = useState<boolean>(false);
-    const [showChoicesForBilling, setShowChoicesForBilling] = useState<boolean>(false);
+export default function CheckoutForm({
+  formInput,
+  setFormInput,
+  existingAddresses,
+  onSubmit,
+  isProcessing,
+}: CheckoutFormProps) {
+  // controls render of new address form for shipping
+  const [showAddShipping, setShowAddShipping] = useState<boolean>(false);
 
+  // controls render of new address form for billing
+  const [showAddBilling, setShowAddBilling] = useState<boolean>(false);
 
-    const handleCheckbox = (evt : ChangeEvent<HTMLInputElement>, role : 'shipping' | 'billing') => {
-        // role -> enum of string options indicating type of address
+  // sets billing same as shipping
+  const [sameAsShipping, setSameAsShipping] = useState<boolean>(false);
 
-        // if checkbox checked...
-        if (evt.target.checked && existingAddresses) {
-                // update formInput by using string value of role to determine which address to update
-                setFormInput((prev) => ({
-                    ...prev,
-                    [role]: existingAddresses[parseInt(evt.target.value)]
-                }));
+  // controls render of list of choices of user's existing addresses
+  const [showChoicesForBilling, setShowChoicesForBilling] =
+    useState<boolean>(false);
 
-        // if checkbox unchecked...
-        } else {
-            // clear formInput by setting corresponding address to empty object
-            setFormInput((prev) => ({
-                ...prev,
-                [role]: {} as AddressType
-            }));
-        }
+  // for rendering shipping/delivery and billing sections
+  const [currentSection, setCurrentSection] = useState<1 | 2>(1);
+
+  const handleCheckbox = (
+    evt: ChangeEvent<HTMLInputElement>,
+    role: 'shipping' | 'billing'
+  ) => {
+    // role -> enum of string options indicating type of address
+
+    // if checkbox checked...
+    if (evt.target.checked && existingAddresses) {
+      // update formInput by using string value of role to determine which address to update
+      setFormInput((prev) => ({
+        ...prev,
+        [role]: existingAddresses[parseInt(evt.target.value)],
+      }));
+
+      // if checkbox unchecked...
+    } else {
+      // clear formInput by setting corresponding address to empty object
+      setFormInput((prev) => ({
+        ...prev,
+        [role]: {} as AddressType,
+      }));
     }
+  };
 
-    const handleMethodSelect = (evt : ChangeEvent<HTMLSelectElement>) => {
-        setFormInput((prev) => ({
-            ...prev,
-            method: evt.target.value
-        }));
-      }
+  const handleMethodSelect = (evt: ChangeEvent<HTMLSelectElement>) => {
+    setFormInput((prev) => ({
+      ...prev,
+      method: evt.target.value,
+    }));
+  };
 
-    const handleSameAsShipping = (evt: React.ChangeEvent<HTMLInputElement>) => {
-        // updates billing address with shipping address info
-        evt.preventDefault();
-        if (evt.target.value === 'yes') {
-            setSameAsShipping(true);
-          setFormInput((prev) => ({
-            ...prev, billing: prev?.shipping
-            }));
-            setShowChoicesForBilling(false);
-            setShowAddBilling(false);
-      } else {
-        setSameAsShipping(false);
-        setShowChoicesForBilling(true);
-      }
+  const handleSameAsShipping = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    // updates billing address with shipping address info
+    evt.preventDefault();
+    if (evt.target.value === 'yes') {
+      setSameAsShipping(true);
+      setFormInput((prev) => ({
+        ...prev,
+        billing: prev?.shipping,
+      }));
+      setShowChoicesForBilling(false);
+      setShowAddBilling(false);
+    } else {
+      setSameAsShipping(false);
+      setShowChoicesForBilling(true);
     }
+  };
 
-    const handleSubmit = async () => {
-        onSubmit();
-    }
+  const handleSubmit = async () => {
+    onSubmit();
+  };
 
+  const renderShippingSection = () => {
     return (
-        <form onSubmit={handleSubmit}>
+      <>
         <div>
           <h3>Delivery Address</h3>
           {existingAddresses.length > 0 && (
@@ -121,30 +141,38 @@ export default function CheckoutForm ({ formInput, setFormInput, existingAddress
             <option value="NEXT_DAY">Next Day</option>
           </select>
         </div>
+        <button onClick={() => setCurrentSection(2)}>Next</button>
+      </>
+    );
+  };
+
+  const renderBillingSection = () => {
+    return (
+      <>
         <div>
           <h3>Billing Address</h3>
           <p>is Billing same as shipping?</p>
           <div>
-  <input
-    type="radio"
-    id="sameAsShipping"
-    name="sameAsShipping"
-    value="yes"
-    checked={sameAsShipping}
-    onChange={(evt) => handleSameAsShipping(evt)}
-  />
-  <label htmlFor="sameAsShipping">Yes</label>
+            <input
+              type="radio"
+              id="sameAsShipping"
+              name="sameAsShipping"
+              value="yes"
+              checked={sameAsShipping === true}
+              onChange={(evt) => handleSameAsShipping(evt)}
+            />
+            <label htmlFor="sameAsShipping">Yes</label>
 
-  <input
-    type="radio"
-    id="notSameAsShipping"
-    name="sameAsShipping"
-    value="no"
-    checked={sameAsShipping}
-    onChange={(evt) => handleSameAsShipping(evt)}
-  />
-  <label htmlFor="notSameAsShipping">No</label>
-</div>
+            <input
+              type="radio"
+              id="notSameAsShipping"
+              name="sameAsShipping"
+              value="no"
+              checked={sameAsShipping === false}
+              onChange={(evt) => handleSameAsShipping(evt)}
+            />
+            <label htmlFor="notSameAsShipping">No</label>
+          </div>
           {showChoicesForBilling && existingAddresses ? (
             <div>
               {existingAddresses.map((address, index) => (
@@ -183,10 +211,19 @@ export default function CheckoutForm ({ formInput, setFormInput, existingAddress
             ''
           )}
         </div>
+        <button onClick={() => setCurrentSection(1)}>Previous</button>
         <input
           type="submit"
           value={isProcessing ? 'Processing...' : 'Continue to Payment'}
         />
-      </form>
+      </>
     );
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {currentSection === 1 && renderShippingSection()}
+      {currentSection === 2 && renderBillingSection()}
+    </form>
+  );
 }
