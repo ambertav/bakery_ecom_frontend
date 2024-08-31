4ff93e243452935a96a834994f92a97f29dd3a33
Amber Taveras <taveras.amber@gmail.com>
2024-08-30 23:09:13 -0400

    removes axios intercepter due to switch to httponly cookies

--- src/utilities/axiosConfig.tsx
+++ src/utilities/axiosConfig.tsx
@@ -1,26 +1,9 @@
 import axios from 'axios'
-import { auth } from '../app/firebase/firebaseConfig';
 
 const instance = axios.create({
     baseURL: 'http://127.0.0.1:5000/api/',
     withCredentials: true,
 });
 
-instance.interceptors.request.use(async (config) => {
-    try {
-        const user = auth.currentUser;
-
-        if (user) {
-            const token = await user.getIdToken();
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-
-        return config;
-
-    } catch (error) {
-        console.error('Error fetching token:', error);
-        return config;
-    }
-});
 
 export default instance;
