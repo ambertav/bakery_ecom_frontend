800ae0733d78b567b7c284b23e37e48bd01b9e6c
Amber Taveras <taveras.amber@gmail.com>
2024-09-05 15:10:22 -0400

    adds deduct stock input, refactors update stock state to hold amount to add or substract from actual

--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -6,6 +6,7 @@ import {
   SetStateAction,
 } from 'react';
 import Link from 'next/link';
+import { useRouter, useSearchParams } from 'next/navigation';
 import {
   FormInput,
   ProductType,
@@ -24,36 +25,33 @@ export default function InventoryItem({
   updatedPortions,
   setUpdatedPortions,
 }: InventoryProps) {
-  const [stockFormInput, setStockFormInput] = useState<FormInput>({});
+    const router = useRouter();
+    const params = useSearchParams();
+  const [incomingInput, setIncomingInput] = useState<FormInput>({});
+  const [deductInput, setDeductInput] = useState<FormInput>({});
 
   useEffect(() => {
-    if (Object.keys(updatedPortions).length !== 0) {
-      const newStockFormInput: FormInput = {};
-
-      // construct stockFormInput to auto populate incoming stock
-        // when updatedPortions is not empty {}
-        // i.e generate report param
-      for (const [productId, portions] of Object.entries(updatedPortions)) {
-        if (Number(productId) === product.id) {
-          for (const [portionId, stock] of Object.entries(portions)) {
-            newStockFormInput[`stock-${portionId}`] = stock as string;
+    if (params?.get('generate-report') === 'true') {
+        if (Object.keys(updatedPortions).length !== 0) {
+          const newStockFormInput: FormInput = {};
+    
+          // construct stockFormInput to auto populate incoming stock
+            // when updatedPortions is not empty {}
+            // i.e generate report param
+          for (const [productId, portions] of Object.entries(updatedPortions)) {
+            if (Number(productId) === product.id) {
+              for (const [portionId, stock] of Object.entries(portions)) {
+                newStockFormInput[`stock-${portionId}-incoming`] = stock as string;
+              }
+            }
           }
+    
+          setIncomingInput(newStockFormInput);
         }
-      }
-
-      setStockFormInput(newStockFormInput);
     }
   }, [updatedPortions, product.id]);
 
-  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => {
-    const portionId = Number(evt.target.name.split('-')[1]);
-    const value = Number(evt.target.value);
-
-    setStockFormInput((prev) => ({
-      ...prev,
-      [evt.target.name]: value,
-    }));
-
+  const handleChange = (portionId : number, value : number) => {
     setUpdatedPortions((prev) => {
       // find the index of the existing product entry
       const productEntry = prev[product.id];
@@ -99,6 +97,48 @@ export default function InventoryItem({
     });
   };
 
+  const handleIncomingChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const portionId = Number(evt.target.name.split('-')[1]);
+    const value = Number(evt.target.value);
+
+    const portion = product.portions.find((portion) => portion.id === portionId);
+
+    if (!portion) return;
+
+    setIncomingInput((prev) => ({
+      ...prev,
+      [evt.target.name]: value,
+    }));
+
+    setDeductInput((prev) => {
+        const { [`stock-${portionId}-deduct`]: _, ...rest } = prev;
+        return rest;
+      });
+
+    return handleChange(portionId, value);
+  }
+
+  const handleDeductChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const portionId = Number(evt.target.name.split('-')[1]);
+    const value = Number(evt.target.value);
+
+    const portion = product.portions.find((portion) => portion.id === portionId);
+
+    if (!portion) return;
+
+    setDeductInput((prev) => ({
+      ...prev,
+      [evt.target.name]: value,
+    }));
+
+    setIncomingInput((prev) => {
+        const { [`stock-${portionId}-incoming`]: _, ...rest } = prev;
+        return rest;
+      });
+
+    return handleChange(portionId, 0 - value);
+  }
+
   return (
     <tr>
       <td>
@@ -119,6 +159,7 @@ export default function InventoryItem({
               <th>Optimal Stock</th>
               <th>Actual Stock</th>
               <th>Incoming Stock</th>
+              {!params?.get('generate-report') && <th>Deduct Stock</th>}
             </tr>
           </thead>
           <tbody>
@@ -127,15 +168,29 @@ export default function InventoryItem({
                 <td>{portion.size}</td>
                 <td>{portion.optimalStock}</td>
                 <td>{portion.stock}</td>
-                <td key={portion.id}>
+                <td key={`incoming-${portion.id}`}>
                   {renderInput(
-                    `stock-${portion.id}`,
+                    `stock-${portion.id}-incoming`,
                     '',
                     'number',
-                    stockFormInput,
-                    handleChange
+                    incomingInput,
+                    handleIncomingChange
                   )}
                 </td>
+                {!params?.get('generate-report') &&
+                <>
+                <td key={`deduct-${portion.id}`}>
+                  {renderInput(
+                    `stock-${portion.id}-deduct`,
+                    '',
+                    'number',
+                    deductInput,
+                    handleDeductChange,
+                    { max : portion.stock }
+                  )}
+                </td>
+                </>
+                }
               </tr>
             ))}
           </tbody>
