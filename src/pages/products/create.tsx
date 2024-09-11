e1ea2c6314d0ca7beb6d22bf8a1a8f2c17e49534
Amber Taveras <taveras.amber@gmail.com>
2024-09-10 23:14:44 -0400

    refactors to make call to create or update, removed need for separate upload photo call

--- src/pages/products/create.tsx
+++ src/pages/products/create.tsx
@@ -3,18 +3,26 @@ import { FormInput } from '../../../types/types';
 import ProductForm from '@/components/ProductForm';
 
 export default function ProductCreate() {
-    const [ formInput, setFormInput ] = useState<FormInput>({
-        name: '',
-        description: '',
-        category: '',
-        price: 1,
-    });
+  const [formInput, setFormInput] = useState<FormInput>({
+    name: '',
+    description: '',
+    category: '',
+    price: 1,
+  });
 
-    const [displayFile, setDisplayFile] = useState<string>('');
-    
-    return (
-        <>
-            <ProductForm formInput={formInput} setFormInput={setFormInput} id={''} isLoading={false} setIsLoading={null} displayFile={displayFile} setDisplayFile={setDisplayFile} />
-        </>
-    );
+  const [displayFile, setDisplayFile] = useState<string>('');
+
+  return (
+    <>
+      <ProductForm
+        formInput={formInput}
+        setFormInput={setFormInput}
+        id={''}
+        isLoading={false}
+        setIsLoading={null}
+        displayFile={displayFile}
+        setDisplayFile={setDisplayFile}
+      />
+    </>
+  );
 }
